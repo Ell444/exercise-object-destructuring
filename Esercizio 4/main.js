12efec07aa54af135e00c04b27c8219efade5add
Laura Sirigu <laurasirigu43@gmail.com>
2023-11-14 14:50:22 +0100

    feat || quarto esercizio

---
+++ Esercizio 4/main.js
@@ -0,0 +1,14 @@
+//### Esercizio 4: Distrutturazione di Oggetti Annidati
+
+//Utilizza la distrutturazione degli oggetti per estrarre la `via` e la `citta` dall'oggetto `indirizzo` all'interno dell'oggetto `utente`.
+
+const utente = {
+    nome: "Alice",
+    indirizzo: {
+        via: "123 Via degli Aceri",
+        citta: "Paese delle Meraviglie"
+    }
+};
+
+const {indirizzo: {via, citta}} = utente;
+
