88204ddc102ab356bb0b5a673908dee15aeb99ab
Laura Sirigu <laurasirigu43@gmail.com>
2023-11-14 14:19:23 +0100

    first commit || primo esercizio fatto

---
+++ Esercizio 1/main.js
@@ -0,0 +1,14 @@
+//Dato il seguente oggetto in JavaScript, convertilo in una stringa JSON.
+
+const utente = {
+    nome: "Alice",
+    eta: 25,
+    hobby: ["leggere", "giocare ai videogiochi", "fare escursioni"],
+    indirizzo: {
+        via: "123 Via degli Aceri",
+        citta: "Paese delle Meraviglie"
+    }
+};
+
+JSON.stringify(utente);
+console.log (JSON.stringify(utente))
